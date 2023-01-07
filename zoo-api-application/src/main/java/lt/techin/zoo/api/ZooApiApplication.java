e4fd9ef9f02e27c05b56958f883cc19c9bc8cae0
P.Sasnauskas <p.sasnauskas@gmail.com>
2023-01-07 13:14:05 +0200

    [ project structure ] Spring initializer generated code

---
+++ zoo-api-application/src/main/java/lt/techin/zoo/api/ZooApiApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.zoo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooApiApplication.class, args);
+	}
+
+}
---
+++ zoo-api-application/src/test/java/lt/techin/zoo/api/ZooApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.zoo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
